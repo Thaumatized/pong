66b55984f7b5cdaf3db83a2e2805eb479e51d704
unknown <aarni.junkkala@gmail.com>
2024-11-21 22:31:38 +0200

    Update pong.js

--- pong.js
+++ pong.js
@@ -9,6 +9,8 @@ var ballAngle = 0;
 var ballPos = [0,0];
 var ballSpeed = 5;
 
+var keys = [0,0];
+
 function start()
 {
     pedal1 = document.getElementById("player1");
@@ -23,36 +25,52 @@ function start()
     pedal2.style.top = 512 / 2 - 128 / 2;
     player2pos = 512 / 2 - 128 / 2;
 
-    ball.style.top = 512 / 2 - 32 / 2;
-    ball.style.left = 512 / 2 - 32 / 2;
-    ballPos[0] = 512 / 2 - 32 / 2;
-    ballPos[1] = 512 / 2 - 32 / 2;
-
-    ballAngle = Math.round(Math.random()) * Math.PI;
-
+    resetBall();
     document.addEventListener('keydown', function(event) {
         if(event.key == "w")
         {
-            move(0,1);
+            keys[0] = 1;
         }
-        if(event.key == "s")
+        else if(event.key == "s")
         {
-            move(0,-1);
+            keys[0] = -1;
         }
         if(event.key == "ArrowUp")
         {
-            move(1,1);
+            keys[1] = 1;
+        }
+        else if(event.key == "ArrowDown")
+        {
+            keys[1] = -1;
+        }
+    });
+    document.addEventListener('keyup', function(event) {
+        if(event.key == "w")
+        {
+            keys[0] = 0;
+        }
+        if(event.key == "s")
+        {
+            keys[0] = 0;
+        }
+        if(event.key == "ArrowUp")
+        {
+            keys[1] = 0;
         }
         if(event.key == "ArrowDown")
         {
-            move(1,-1);
+            keys[1] = 0;
         }
     });
+
     setInterval(update, 1000 / 60);
 }
 
 function update()
 {
+    move(0,keys[0]);
+    move(1,keys[1]);
+
     console.log(ballAngle);
     var oldPos = ballPos.slice();
     ballPos[0] += Math.cos(ballAngle) * ballSpeed;
@@ -64,9 +82,9 @@ function update()
         oldPos[0] > (512 - 8 - 32 - 32) &&
         ballPos[0] >= (512 - 8 - 32 - 32) &&
         ballPos[1] > player2pos &&
-        ballPos[1] < player2pos + 128)
+        ballPos[1] < player2pos + 128 - 32)
     {
-        ballAngle = (ballAngle + Math.PI + Math.random() * 0.25 * Math.PI) % (Math.PI * 2);
+        ballAngle = (ballAngle + Math.PI + (Math.random() - 0.5) * 0.25 * Math.PI) % (Math.PI * 2);
     }
 
     //Collider to left
@@ -78,14 +96,43 @@ function update()
         ballPos[1] > player1pos &&
         ballPos[1] < player1pos + 128)
     {
-        ballAngle = (ballAngle + Math.PI + Math.random() * 0.25 * Math.PI) % (Math.PI * 2);
+        ballAngle = (ballAngle + Math.PI + (Math.random() - 0.5) * 0.25 * Math.PI) % (Math.PI * 2);
     }
     
-    //Topwall
-    
+    //Topwall   y angle is flipped
+    if(oldPos[1] > 0 && ballPos[1] <= 0 && ballAngle > Math.PI && ballAngle < Math.PI * 2)
+    {
+        ballAngle += (Math.PI - ballAngle) * 2
+    }
+
+    //Bottom
+    if(oldPos[1] > 512 - 32 && ballPos[1] <= 512 - 32 && ballAngle > 0 && ballAngle < Math.PI)
+    {
+        ballAngle += (Math.PI - ballAngle) * 2
+    }
 
     ball.style.left = ballPos[0];
     ball.style.top = ballPos[1];
+
+    if(ballPos[0] < 0)
+    {
+        console.log("Player 2 won!");
+        resetBall();
+    }
+    if(ballPos[0] > 512)
+    {
+        console.log("Player 1 won!");
+        resetBall();
+    }
+}
+
+function resetBall()
+{
+    ballPos[0] = 512 / 2 - 32 / 2;
+    ballPos[1] = 512 / 2 - 32 / 2;
+    ball.style.top = ballPos[1];
+    ball.style.left = ballPos[0];
+    ballAngle = Math.round(Math.random()) * Math.PI;
 }
 
 function move(index,dir)
